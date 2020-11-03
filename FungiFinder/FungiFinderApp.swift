67e6ef9d62a5bbc34d80480916d2d9605f567135
Mohammad Azam <azamsharp@gmail.com>
2020-11-03 06:22:01 -0600

    Initial Commit

---
+++ FungiFinder/FungiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FungiFinderApp.swift
+//  FungiFinder
+//
+//  Created by Mohammad Azam on 11/3/20.
+//
+
+import SwiftUI
+
+@main
+struct FungiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
